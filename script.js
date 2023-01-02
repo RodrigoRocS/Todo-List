95ede6d81653cacb581e5f809483c6ea1a9d2c48
RodrigoRocS <rrsilva1701@gmail.com>
2023-01-02 10:36:00 -0300

    requisito 1 concluido

---
+++ script.js
@@ -0,0 +1,11 @@
+const pegaBody = document.getElementsByTagName('body')[0];
+
+const criaHeader = () => {
+  const headerDaGalera = document.createElement('header');
+  headerDaGalera.innerText = 'Minha Lista de Tarefas';
+  pegaBody.appendChild(headerDaGalera);
+};
+
+window.onload = () => {
+  criaHeader();
+};
